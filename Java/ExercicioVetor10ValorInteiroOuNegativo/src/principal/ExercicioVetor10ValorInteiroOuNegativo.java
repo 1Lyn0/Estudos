1fa65463725de77b462b31464bffae50bfe324c5
unknown <jvic.coel@gmail.com>
2025-12-09 22:14:32 -0300

    Primeiro exercicio com vetor

---
+++ Java/ExercicioVetor10ValorInteiroOuNegativo/src/principal/ExercicioVetor10ValorInteiroOuNegativo.java
@@ -0,0 +1,24 @@
+// Exercício 01: Escreva um algoritmo que leia 10 valores inteiros positivos ou negativos e armazene-os em um vetor.
+// Ao digitar 0, o sistema deve ignorar o valor lido e solicitar um novo valor
+// Após a leitura o algoritmo deve copiar os valores positivos para um vetor de números positivos e os negativos para o vetor de números negativos.
+// Ao término o algoritmo deve exibir os valores do vetor positivo e os valores do vetor negativo, além de informar o percental de números negativos e positivos.
+
+package principal;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ExercicioVetor10ValorInteiroOuNegativo{
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int[] numeros = new int[10];
+
+        System.out.println("Digite 10 números inteiros: ");
+        numeros = teclado.nextInt();
+
+        if ()
+
+
+        teclado.close();
+    }
+}
